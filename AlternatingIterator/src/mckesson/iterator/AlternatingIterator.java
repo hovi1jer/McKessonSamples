64e7f2322299641d70f6960cbb61a38fd9a14394
Administrator <Administrator@Jeremy-PC>
2018-10-04 17:59:14 -0500

    Initial Commit Of Implementation

---
+++ AlternatingIterator/src/mckesson/iterator/AlternatingIterator.java
@@ -0,0 +1,119 @@
+package mckesson.iterator;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
+
+/**
+ * @author jhovis
+ * 
+ * Alternates in order between the iterators received in the constructor
+ *
+ * @param <T>
+ */
+public class AlternatingIterator<T> implements Iterator<T>
+{
+	private List<Iterator<T>> hasNextIterators = new ArrayList<Iterator<T>>();
+	private List<Iterator<T>> nextIterators = new ArrayList<Iterator<T>>();
+	private ListIterator<Iterator<T>> hasNextIteratorsIter = null;
+	private ListIterator<Iterator<T>> nextIteratorsIter = null;
+	
+	/**
+	 * Create an instance of an alternating iterator
+	 * 
+	 * @param iterators
+	 */
+	@SafeVarargs
+	public AlternatingIterator(Iterator<T>... iterators) 
+	{
+		// The iterators that are passed in can not be used directly as the references will be modified the 
+		// hasNext and Next methods so copies need to be made so we don't unexpectedly move an iterator 
+		for(Iterator<T> iterator : iterators) {
+			if (iterator != null)
+			{
+				List<T> list = new ArrayList<T>();
+				iterator.forEachRemaining(value -> {
+					list.add(value);
+				});
+				
+				hasNextIterators.add(list.listIterator());
+				nextIterators.add(list.listIterator());
+			}
+		}
+		
+		hasNextIteratorsIter = hasNextIterators.listIterator();
+		nextIteratorsIter = nextIterators.listIterator();
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.util.Iterator#hasNext()
+	 */
+	@Override
+	public boolean hasNext()
+	{
+		boolean hasNextIteratorsIterHasNext = hasNextIteratorsIter.hasNext();
+		boolean innerHasNext = false;
+		
+		// loop until all the has next iterators and the item iterators have been exhausted
+		while (hasNextIteratorsIterHasNext || innerHasNext)
+		{
+			Iterator<T> innerIter = hasNextIteratorsIter.next();
+			innerHasNext = innerIter.hasNext();
+			if (innerHasNext)
+			{
+				// if the end of the has next iterators is reached then reset it 
+				// to the beginning as the last iterator value is being retrieved
+				if (!hasNextIteratorsIter.hasNext())
+				{
+					hasNextIteratorsIter = hasNextIterators.listIterator();
+				}
+				
+				innerIter.next();
+				return innerHasNext;
+			}
+			else
+			{
+				hasNextIteratorsIterHasNext = hasNextIteratorsIter.hasNext();
+			}
+		}
+		
+		return false;
+	}
+	
+	
+
+	@Override
+	/* (non-Javadoc)
+	 * @see java.util.Iterator#next()
+	 */
+	public T next() 
+	{
+		boolean nextIteratorsIterHasNext = nextIteratorsIter.hasNext();
+		boolean innerHasNext = false;
+		
+		// loop until all the next iterators and the item iterators have been exhausted
+		while (nextIteratorsIterHasNext || innerHasNext)
+		{
+			Iterator<T> innertIter = nextIteratorsIter.next();		
+			innerHasNext = innertIter.hasNext();
+			if (innerHasNext)
+			{
+				// if the end of the next iterators is reached then reset it 
+				// to the beginning as the last iterator value is being retrieved
+				if (!nextIteratorsIter.hasNext())
+				{
+					nextIteratorsIter = nextIterators.listIterator();
+				}
+				
+				return innertIter.next();
+			}
+			else
+			{
+				nextIteratorsIterHasNext = nextIteratorsIter.hasNext();
+			}
+		}
+		
+		throw new NoSuchElementException("Out Of Bounds - No Element Found");
+	}
+}
---
+++ AlternatingIterator/src/mckesson/iterator/AlternatingIteratorTest.java
@@ -0,0 +1,219 @@
+package mckesson.iterator;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.junit.jupiter.api.Test;
+
+class AlternatingIteratorTest {
+	
+	@Test
+	public void testAllNullIterators() {
+		
+		AlternatingIterator<String> testIter = new AlternatingIterator<String>(null, null, null);
+		
+		assertEquals(false, testIter.hasNext());
+	    assertThrows(NoSuchElementException.class, () -> {
+	    	testIter.next();
+	    });
+	}
+	
+	@Test
+	public void testSomeNullIterators() {
+		
+		List<String> list1 = Arrays.asList("a","b", "c");
+		List<String> list2 = Arrays.asList("1", "2");
+		List<String> list3 = Arrays.asList("x", "y", "z");
+		
+		AlternatingIterator<String> testIter = new AlternatingIterator<String>(null, list1.iterator(), null, list2.iterator(), null, list3.iterator());
+		
+		assertEquals(true, testIter.hasNext());
+		assertEquals("a", testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals("1", testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals("x", testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals("b", testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals("2", testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals("y", testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals("c", testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals("z", testIter.next());
+		assertEquals(false, testIter.hasNext());
+	    assertThrows(NoSuchElementException.class, () -> {
+	    	testIter.next();
+	    });
+	}
+	
+	@Test
+	public void testAllEmptyIterators() {
+		
+		List<String> list1 = Arrays.asList();
+		List<String> list2 = Arrays.asList();
+		List<String> list3 = Arrays.asList();
+		
+		AlternatingIterator<String> testIter = new AlternatingIterator<String>(list1.iterator(), list2.iterator(), list3.iterator());
+		
+		assertEquals(false, testIter.hasNext());
+	    assertThrows(NoSuchElementException.class, () -> {
+	    	testIter.next();
+	    });
+	}
+	
+	@Test
+	public void testSomeEmptyIterators() {
+		
+		List<String> list1 = Arrays.asList("a","b", "c");
+		List<String> list2 = Arrays.asList("1", "2");
+		List<String> list3 = Arrays.asList("x", "y", "z");
+		List<String> emptyList = Arrays.asList();
+		
+		AlternatingIterator<String> testIter = new AlternatingIterator<String>(emptyList.iterator(), list1.iterator(), emptyList.iterator(), list2.iterator(), emptyList.iterator(), list3.iterator());
+		
+		assertEquals(true, testIter.hasNext());
+		assertEquals("a", testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals("1", testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals("x", testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals("b", testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals("2", testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals("y", testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals("c", testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals("z", testIter.next());
+		assertEquals(false, testIter.hasNext());
+	    assertThrows(NoSuchElementException.class, () -> {
+	    	testIter.next();
+	    });
+	}
+	
+	@Test
+	public void testStringHasNextMethodOnly() {
+		
+		List<String> list1 = Arrays.asList("a", "", "b", "c");
+		List<String> list2 = Arrays.asList("1", "2");
+		List<String> list3 = Arrays.asList("x", null, "y", "z");
+		
+		AlternatingIterator<String> testIter = new AlternatingIterator<String>(list1.iterator(), list2.iterator(), list3.iterator());
+		
+		assertEquals(true, testIter.hasNext());
+		assertEquals(true, testIter.hasNext());
+		assertEquals(true, testIter.hasNext());
+		assertEquals(true, testIter.hasNext());
+		assertEquals(true, testIter.hasNext());
+		assertEquals(true, testIter.hasNext());
+		assertEquals(true, testIter.hasNext());
+		assertEquals(true, testIter.hasNext());
+		assertEquals(true, testIter.hasNext());
+		assertEquals(true, testIter.hasNext());
+		assertEquals(false, testIter.hasNext());
+	}
+	
+	@Test
+	public void testStringNextMethodOnly() {
+		
+		List<String> list1 = Arrays.asList("a", "", "b", "c");
+		List<String> list2 = Arrays.asList("1", "2");
+		List<String> list3 = Arrays.asList("x", null, "y", "z");
+		
+		AlternatingIterator<String> testIter = new AlternatingIterator<String>(list1.iterator(), list2.iterator(), list3.iterator());
+		
+		assertEquals("a", testIter.next());
+		assertEquals("1", testIter.next());
+		assertEquals("x", testIter.next());
+		assertEquals("", testIter.next());
+		assertEquals("2", testIter.next());
+		assertEquals(null, testIter.next());
+		assertEquals("b", testIter.next());
+		assertEquals("y", testIter.next());
+		assertEquals("c", testIter.next());
+		assertEquals("z", testIter.next());
+	    assertThrows(NoSuchElementException.class, () -> {
+	    	testIter.next();
+	    });
+	}
+
+	@Test
+	public void testStringHasNextAndNextMethods() {
+		
+		List<String> list1 = Arrays.asList("a", "", "b", "c");
+		List<String> list2 = Arrays.asList("1", "2");
+		List<String> list3 = Arrays.asList("x", null, "y", "z");
+		
+		AlternatingIterator<String> testIter = new AlternatingIterator<String>(list1.iterator(), list2.iterator(), list3.iterator());
+		
+		assertEquals(true, testIter.hasNext());
+		assertEquals("a", testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals("1", testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals("x", testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals("", testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals("2", testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals(null, testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals("b", testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals("y", testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals("c", testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals("z", testIter.next());
+		assertEquals(false, testIter.hasNext());
+	    assertThrows(NoSuchElementException.class, () -> {
+	    	testIter.next();
+	    });
+	}
+	
+	@Test
+	public void testIntegerHasNextAndNextMethods() {
+		
+		List<Integer> list1 = Arrays.asList(-2, 1, 4, 6);
+		List<Integer> list2 = Arrays.asList(-1, 2);
+		List<Integer> list3 = Arrays.asList(0, 3, 5, 7, 8);
+		
+		AlternatingIterator<Integer> testIter = new AlternatingIterator<Integer>(list1.iterator(), list2.iterator(), list3.iterator());
+		
+		assertEquals(true, testIter.hasNext());
+		assertEquals(new Integer(-2), testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals(new Integer(-1), testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals(new Integer(0), testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals(new Integer(1), testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals(new Integer(2), testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals(new Integer(3), testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals(new Integer(4), testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals(new Integer(5), testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals(new Integer(6), testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals(new Integer(7), testIter.next());
+		assertEquals(true, testIter.hasNext());
+		assertEquals(new Integer(8), testIter.next());
+		assertEquals(false, testIter.hasNext());
+	    assertThrows(NoSuchElementException.class, () -> {
+	    	testIter.next();
+	    });
+	}
+}
